8bb74f903998efd02f38630a159842c35011e232
Ainsley O'Garro <aogarro1@fb.com>
2020-07-06 17:56:17 -0400

    Initial commit Allows user to sign in to account

---
+++ app/src/main/java/com/example/parstegram/ParseApplication.java
@@ -0,0 +1,40 @@
+package com.example.parstegram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for troubleshooting -remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for mointoring Parse OkHttp traffic
+
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId, and server server based on the values in the Heroku settings
+
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ainsley-parstagram")
+                .clientKey("TeamMe7o")
+                .clientBuilder(builder)
+                .server("https://ainsley-parstagram.herokuapp.com/parse/").build());
+
+        //ParseObject testObject = new ParseObject("TestObject");
+        //testObject.put("foo", "bar");
+        //testObject.saveInBackground();
+    }
+}
